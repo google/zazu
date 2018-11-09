7834daed34ccf03b0416db31c7c85d323cd17bcd
Maria Attarian <jmattarian@jmattarian-macbookpro.roam.corp.google.com>
2018-11-09 16:04:58 -0500

    Edit organization API

--- src/routes/api.js
+++ src/routes/api.js
@@ -529,6 +529,20 @@ router.post('/deleteOrganization', function(req, res) {
   });
 });
 
+router.post('/editOrganization', function(req, res) {
+
+  var editOrg = req.body;
+
+  Organization.updateOne({ _id : editOrg._id }, editOrg, function(err, result) {
+    if (err) {
+      res.send({ status: '500', message: 'Organization failed to update.' });
+    } else {
+      res.send({ status: '200', result });
+    }
+  });
+
+});
+
 router.get('/getAllReports', function(req, res) {
   Report.find(function(err, docs) {
     if (err) {
