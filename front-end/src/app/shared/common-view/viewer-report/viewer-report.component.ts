094213fd954b7e3b840a5eea0f150b2ac5b52851
eldon <eldon.gonio@gmail.com>
2019-01-08 17:03:03 -0500

    added regex changes in viewer report view

--- front-end/src/app/shared/common-view/viewer-report/viewer-report.component.ts
+++ front-end/src/app/shared/common-view/viewer-report/viewer-report.component.ts
@@ -51,7 +51,7 @@ export class ViewerReportComponent implements OnInit, OnDestroy {
         const replaceLink = this.report.link.replace(patt, '/embed/reporting');
         this.embedLink = this.sanitizer.bypassSecurityTrustResourceUrl(replaceLink);
         */
-        const patt = new RegExp('google\.com\/(.)+\/reporting');
+        const patt = new RegExp('google\.com(.)*\/reporting');
         const replaceLink = this.report.link.replace(patt, 'google.com/embed/reporting');
         this.embedLink = this.sanitizer.bypassSecurityTrustResourceUrl(replaceLink);
 
--- src/utilities/config.js
+++ src/utilities/config.js
@@ -9,8 +9,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-
-
 module.exports = {
     bq_instance:  process.env.bq_instance,
     bq_dataset: process.env.bq_dataset,
