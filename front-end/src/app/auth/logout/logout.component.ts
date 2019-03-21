7d021dc47dfff037647b4d7ed77d213d18884a34
Maria Attarian <jmattarian@jmattarian-macbookpro.roam.corp.google.com>
2019-03-21 11:34:41 -0400

    proper gapi logout

--- front-end/src/app/auth/logout/logout.component.ts
+++ front-end/src/app/auth/logout/logout.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './../auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-logout',
@@ -9,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor(private auth: AuthService, private http: HttpClient) { }
+  constructor(private auth: AuthService, private ngZone: NgZone, private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
 
   companyName;
+
+  public returnToLogin() {
+    var _this = this;
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+            console.log('User signed out.');
+            auth2.disconnect();
+            _this.ngZone.run(() => _this.router.navigate(['../login'], {relativeTo: _this.route})).then();
+    });
+  }
 
   async ngOnInit() {
     const call = await <any> this.http.get('../../assets/main-variables.json').toPromise();
